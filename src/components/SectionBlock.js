0342632ed235eabdbea5e4af23a1280ac584929b
Ejike Victory <victoryejike@gmail.com>
2023-01-05 09:09:15 +0100

    fix: weirdest issue i have ever fixed solution was to change sticky to fixed

--- src/components/SectionBlock.js
+++ src/components/SectionBlock.js
@@ -10,14 +10,14 @@ const SectionBlock = () => {
       bgUrl:
         "https://tesla-cdn.thron.com/delivery/public/image/tesla/03e533bf-8b1d-463f-9813-9a597aafb280/bvlatuR/std/4096x2560/M3-Homepage-Desktop-LHD"
     },
-    // {
-    //   title: "Model S",
-    //   subTitle: "Schedule a Demo Drive",
-    //   firstActionBtn: "Buy Now",
-    //   secondActionBtn: "Custom Order",
-    //   bgUrl:
-    //     "https://tesla-cdn.thron.com/delivery/public/image/tesla/538ac149-d103-4834-9d38-641d8ae447ef/bvlatuR/std/4096x2560/Homepage-Model-S-Desktop-LHD"
-    // },
+    {
+      title: "Model S",
+      subTitle: "Schedule a Demo Drive",
+      firstActionBtn: "Buy Now",
+      secondActionBtn: "Custom Order",
+      bgUrl:
+        "https://tesla-cdn.thron.com/delivery/public/image/tesla/538ac149-d103-4834-9d38-641d8ae447ef/bvlatuR/std/4096x2560/Homepage-Model-S-Desktop-LHD"
+    },
     {
       title: "Model X",
       subTitle: "Schedule a Demo Drive",
@@ -25,31 +25,31 @@ const SectionBlock = () => {
       secondActionBtn: "Custom Order",
       bgUrl:
         "https://tesla-cdn.thron.com/delivery/public/image/tesla/ddc135ed-1638-40fb-8ab1-f8045059ecef/bvlatuR/std/4096x2560/Homepage-Model-X-Desktop-LHD"
+    },
+    {
+      title: "Solar Panels",
+      subTitle: "Lowest Cost Solar Panels in America",
+      firstActionBtn: "Order Now",
+      secondActionBtn: "Learn More",
+      bgUrl:
+        "https://tesla-cdn.thron.com/delivery/public/image/tesla/16b04537-a4be-4bf9-8637-86862a858da8/bvlatuR/std/2880x1800/_25-HP-SolarPanels-D"
+    },
+    {
+      title: "Solar Roof",
+      subTitle: "Produce Clean Energy From Your Roof",
+      firstActionBtn: "Order Now",
+      secondActionBtn: "Learn More",
+      bgUrl:
+        "https://tesla-cdn.thron.com/delivery/public/image/tesla/4a41ec0e-7316-46a8-8ce9-e2f195ba644d/bvlatuR/std/2880x1800/Homepage-SolarRoof-Desktop-Global?quality=auto-medium&format=auto"
+    },
+    {
+      title: "Accessories",
+      subTitle: "Produce Clean Energy From Your Roof",
+      firstActionBtn: "Shop Now",
+      // secondActionBtn: "Learn More",
+      bgUrl:
+        "https://tesla-cdn.thron.com/delivery/public/image/tesla/dd739764-bcaa-4263-9488-8c73bc9fb046/bvlatuR/std/0x0/dd739764-bcaa-4263-9488-8c73bc9fb046"
     }
-    // {
-    //   title: "Solar Panels",
-    //   subTitle: "Lowest Cost Solar Panels in America",
-    //   firstActionBtn: "Order Now",
-    //   secondActionBtn: "Learn More",
-    //   bgUrl:
-    //     "https://tesla-cdn.thron.com/delivery/public/image/tesla/16b04537-a4be-4bf9-8637-86862a858da8/bvlatuR/std/2880x1800/_25-HP-SolarPanels-D"
-    // },
-    // {
-    //   title: "Solar Roof",
-    //   subTitle: "Produce Clean Energy From Your Roof",
-    //   firstActionBtn: "Order Now",
-    //   secondActionBtn: "Learn More",
-    //   bgUrl:
-    //     "https://tesla-cdn.thron.com/delivery/public/image/tesla/4a41ec0e-7316-46a8-8ce9-e2f195ba644d/bvlatuR/std/2880x1800/Homepage-SolarRoof-Desktop-Global?quality=auto-medium&format=auto"
-    // },
-    // {
-    //   title: "Accessories",
-    //   subTitle: "Produce Clean Energy From Your Roof",
-    //   firstActionBtn: "Shop Now",
-    //   // secondActionBtn: "Learn More",
-    //   bgUrl:
-    //     "https://tesla-cdn.thron.com/delivery/public/image/tesla/dd739764-bcaa-4263-9488-8c73bc9fb046/bvlatuR/std/0x0/dd739764-bcaa-4263-9488-8c73bc9fb046"
-    // }
   ];
   return (
     <div className="h-screen -my-[4.6rem]">
