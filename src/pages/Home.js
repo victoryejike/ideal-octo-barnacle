7b06b647a7c377e1779e464911521b168f26657b
Ejike Victory <victoryejike@gmail.com>
2023-01-20 22:50:48 +0100

    feat: selecting users from user reducer to use in redirecting users to appropraite page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
 import SectionBlock from "../components/SectionBlock";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
 
 const Home = () => {
   const [isMenuOpen, setisMenuOpen] = useState(false);
+  const user = useSelector(selectUser);
   return (
     <div>
       <Header isMenuOpen={isMenuOpen} setisMenuOpen={setisMenuOpen} />
