a3bf50c9fc28ca8321095cf1f2e4cfb556616407
Ejike Victory <victoryejike@gmail.com>
2023-01-21 23:05:43 +0100

    feat: added the header to the teslaaccount page

--- src/pages/Teslaaccount.js
+++ src/pages/Teslaaccount.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../components/Header";
 
 const Teslaaccount = () => {
-  return <div>Teslaaccount</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Teslaaccount;
