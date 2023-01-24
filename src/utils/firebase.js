bc264b06f2eb04bd7d1d5acfbb0be764eeffa01e
Ejike Victory <victoryejike@gmail.com>
2023-01-24 21:49:20 +0100

    feat: added the firebse package and the init file

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5VuhpdV3URljgStFkQQfsB46xwlLp8mc",
+  authDomain: "tesla-clone-46356.firebaseapp.com",
+  projectId: "tesla-clone-46356",
+  storageBucket: "tesla-clone-46356.appspot.com",
+  messagingSenderId: "161201421021",
+  appId: "1:161201421021:web:20494f77494f2107c07934"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
