6e2be3fc44196f392b0ed3f07dd580b9cc92ef49
Ejike Victory <victoryejike@gmail.com>
2023-02-03 23:12:45 +0100

    feat: disable button until all input is entered correctly

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -3,60 +3,46 @@ import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import { logoLink } from "../utils/constants";
 import { MdLanguage } from "react-icons/md";
-import { auth } from "../utils/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+// import { auth } from "../utils/firebase";
+// import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const Signup = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  // const [showPassword, setShowPassword] = useState(false);
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  // const [showSubmitButton, setShowSubmitButton] = useState(false);
   // const [inputType, setInputType] = useState("password");
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
+  const handleFnameChange = (e) => {
+    setFname(e.target.value);
   };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
+  const handleLnameChange = (e) => {
+    setLname(e.target.value);
   };
 
-  // const showPasswordField = (e) => {
-  //   e.preventDefault();
-  //   const emailValidator = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  //   if (email.match(emailValidator)) {
-  //     setShowPassword(true);
-  //   } else alert("Please enter a valid email");
-  // };
-
-  // const changeInputType = () => {
-  //   if (inputType === "password") {
-  //     setInputType("text");
-  //     return;
+  // const showButton = () => {
+  //   if (fname !== '' || lname !== '') {
+  //     setShowSubmitButton(true);
+  //   } else {
+  //     showSubmitButton(false);
   //   }
-  //   setInputType("password");
-  // };
+  // }
 
-  // const changeEmailValue = (e) => {
+  // const handleSubmit = (e) => {
   //   e.preventDefault();
-  //   setShowPassword(false);
-  //   setEmail("");
+  //   console.log(email, password);
+  //   createUserWithEmailAndPassword(auth, email, password)
+  //     .then((userCredential) => {
+  //       // Signed in
+  //       const user = userCredential.user;
+  //       console.log(user);
+  //     })
+  //     .catch((error) => {
+  //       const errorCode = error.code;
+  //       const errorMessage = error.message;
+  //       console.log(errorMessage, errorCode);
+  //     });
   // };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(email, password);
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        console.log(user);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorMessage, errorCode);
-      });
-  };
   return (
     <section className="flex flex-col h-screen">
       <header className="flex justify-between items-center p-4 pl-7">
@@ -70,7 +56,7 @@ const Signup = () => {
 
       <div className="w-3/4 lg:w-3/12 2xl:w-[17%] mx-auto pt-5">
         <h2 className="text-left text-[34px] font-normal  pb-2">Sign In</h2>
-        <form onSubmit={handleSubmit}>
+        <form>
           <div className="pt-6 pb-4">
             <div>
               <div className="flex justify-start items-center">
@@ -78,8 +64,8 @@ const Signup = () => {
               </div>
               <input
                 type="text"
-                value={email}
-                onChange={handleEmailChange}
+                value={fname}
+                onChange={handleFnameChange}
                 className="bg-[#F4F4F4] mt-2 text-sm p-[6px] text-black w-full outline-none rounded border border-transparent focus:border focus:border-gray-300 transition-all"
               />
             </div>
@@ -88,8 +74,8 @@ const Signup = () => {
             <label className="block text-sm text-[#969393]">Last Name</label>
             <input
               type="text"
-              value={password}
-              onChange={handlePasswordChange}
+              value={lname}
+              onChange={handleLnameChange}
               className="bg-[#F4F4F4] relative mt-2 text-sm p-[6px] text-black w-full outline-none rounded border border-transparent focus:border focus:border-gray-300 transition-all"
             />
           </div>
@@ -97,7 +83,13 @@ const Signup = () => {
             By continuing, you agree to our privacy policy and terms of use.
           </span>
           <div className="my-10">
-            <button className=" text-[#393C41] bg-[#F4F4F4] rounded w-full py-2 text-sm">
+            <button
+              type="button"
+              className={`${
+                fname === "" || lname === ""
+                  ? "bg-[#9CB5F1] cursor-not-allowed"
+                  : "bg-[#3E6AE1] cursor-pointer"
+              } text-white rounded w-full py-[9px] text-sm`}>
               Next
             </button>
           </div>
